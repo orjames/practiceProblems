b0940761565084cb342cd9d0419122819fa2dd9e
orjames <orjames@berkeley.edu>
2019-04-29 10:09:11 -0700

    more practice problems

--- bigO.ts
+++ bigO.ts
@@ -1,39 +1,39 @@
-let test = [0, 1, 2, 3, 4, 5];
-let randomTest: number[] = [2, 3, 6, 10, 11];
+let test = [0,1,2,3,4,5]
+let randomTest: number[] = [2,3,6,10,11]
 
 //1-----------------------------------------------------------------------
 const foo = (array: number[]) => {
-  let sum: number = 0;
-  let product: number = 1;
+  let sum: number = 0
+  let product: number = 1
   for (let i: number = 0; i < array.length; i++) {
-    sum += array[i];
+    sum += array[i]
   }
   for (let i: number = 0; i < array.length; i++) {
-    product *= array[i];
+    product *= array[i]
   }
-};
+}
 // do this and then when you're done, do that
 // This is O(N) time complexity, it will take O(N) time, the fact that we iterate through it twice doesn't matter
 
 //2------------------------------------------------------------------------
-const printPairs = (array: number[]) => {
-  for (let i: number = 0; i < array.length; i++) {
-    for (let j: number = 0; j < array.length; j++) {
-      console.log(array[i] + ', ' + array[j]);
-    }
-  }
-};
+ const printPairs = (array: number[]) => {
+   for (let i: number = 0; i < array.length; i++) {
+     for (let j: number = 0; j < array.length; j++) {
+       console.log(array[i] + ', ' + array[j])
+     }
+   }
+ }
 //  console.log(printPairs(array))
-// do this for each time you do that, ie do the inner loop completely for each time you do the outer loop, so you multiply run times O(N) * O(N) therfore it is O(N^2) complexity
+ // do this for each time you do that, ie do the inner loop completely for each time you do the outer loop, so you multiply run times O(N) * O(N) therfore it is O(N^2) complexity
 
 //3------------------------------------------------------------------------
-const printUnrderedPairs = (arry: number[]) => {
-  for (let i: number = 0; i < arry.length; i++) {
-    for (let j: number = i + 1; j < arry.length; j++) {
-      console.log(arry[i], arry[j]);
-    }
-  }
-};
+ const printUnrderedPairs = (arry: number[]) => {
+   for (let i: number = 0; i < arry.length; i++) {
+     for (let j: number = i + 1; j < arry.length; j++) {
+       console.log(arry[i], arry[j])
+     }
+   }
+ }
 // do this for each time you do that, but the thing you're doing is less and less in length each time. The inner for loop with j is dependent on the variable i which is defined in the outer loop. This means you are doing the inner loop less and less each time you do one loop of i.
 // The first time j runs for N-1 steps, the second time it runs though N-2 steps, then N-3, and so on...
 // number of steps is (N-1) + (N-2) + (N-3) + ... + 2 + 1
@@ -60,16 +60,17 @@ const printUnrderedPairs = (arry: number[]) => {
 // which looks like half the N * N matrix, roughly N^2/2 therefore it takes O(n^2) time
 // For average work, the outer loop runs N times. But what about the inner loop. Say N = 10, the inner loop would run 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 times. The average of that is 5 or N/2. So the work would be innerloop * outerloop => N/2 * N = (N^2)/2 or O(N^2)
 
+
 //4-------------------------------------------------------------------------
 const printUnorderedPairs4 = (arrayA: number[], arrayB: number[]) => {
   for (let i: number = 0; i < arrayA.length; i++) {
     for (let j: number = 0; j < arrayB.length; j++) {
       if (arrayA[i] < arrayB[j]) {
-        console.log(arrayA[i] + ',' + arrayB[j]);
+        console.log(arrayA[i] + ',' + arrayB[j])
       }
     }
   }
-};
+}
 // do this for every time you do that
 // the inner loop if statement is O(1) time since its a sequence of constant time events, we can refactor our function:
 const printUnorderedPairs4a = (arrayA: number[], arrayB: number[]) => {
@@ -78,19 +79,20 @@ const printUnorderedPairs4a = (arrayA: number[], arrayB: number[]) => {
       /* O(1) work */
     }
   }
-};
+}
 // So now we can say the for each element in arrayA, the inner loop goes through b iterations, where b = arrayB.length. If a = arrayA.length then the runtime is O(a*b). THIS IS NOT O(n^2) complexity because there are different inputs, both matter. This is an extremely common mistake. They depend on different inputs, arrayA and arrayB so it is O(a*b) complexity.
+
 
 //5-------------------------------------------------------------------------
 const prntUnorderedPairs5 = (arrayA: number[], arrayB: number[]) => {
   for (let i: number = 0; i < arrayA.length; i++) {
-    for (let j: number = 0; j < arrayB.length; j++) {
+    for (let j: number =0; j < arrayB.length; j++) {
       for (let k: number = 0; k < 100000; k++) {
         console.log(arrayA[i] + ', ' + arrayB[j]);
       }
     }
   }
-};
+}
 // Innermost loop happens a set number of times, 100,000 tmes which is a constant time. So we can rewrite this as:
 const prntUnorderedPairs5a = (arrayA: number[], arrayB: number[]) => {
   for (let i: number = 0; i < arrayA.length; i++) {
@@ -98,8 +100,10 @@ const prntUnorderedPairs5a = (arrayA: number[], arrayB: number[]) => {
       /* O(100,000) work */
     }
   }
-};
+}
 // so outermost loop is runs arrayA.length times, call it a times. second loop runs arrayB.length times, call it b times. Innermost loop runs 100,000 times. So its O(a*b*10000) which is just O(a*b)
+
+
 
 //6-------------------------------------------------------------------------
 const reverse2 = (array: number[]) => {
@@ -110,20 +114,23 @@ const reverse2 = (array: number[]) => {
     array[oppositeIndex] = temp;
   }
   console.log(array);
-};
-// this reverses an array
+}
+// this reverses an array 
 // this only goes through the array half of the times so it takes N/2 times to run but this is still O(N) time, the half does not impact big O time.
+
 
 //7-------------------------------------------------------------------------
 // which of the following are equivalent to O(N)? Why?
 'O(N + P), where P < N/2';
-// this is equivalent because it is essentially N + N/2 which is N + N which can just be O(N), dropping the non dominant terms which is O(P)
+  // this is equivalent because it is essentially N + N/2 which is N + N which can just be O(N), dropping the non dominant terms which is O(P)
 'O(2N)';
-// this is equivalent to O(N) because you drop the constants
+  // this is equivalent to O(N) because you drop the constants
 'O(N + logN)';
-// this is equivalent to O(N) because you drop non dominant terms
+  // this is equivalent to O(N) because you drop non dominant terms
 'O(N + M)';
-// there is no established relationship between N and M therefore we can't drop either, therefore the algorithm is of O(N + M) complexity
+  // there is no established relationship between N and M therefore we can't drop either, therefore the algorithm is of O(N + M) complexity
+
+
 
 //8-------------------------------------------------------------------------
 'suppose we had an algorithm that took an array of strings, sorted each string, then sorted the full array. What would the runtime be?';
@@ -134,10 +141,12 @@ const reverse2 = (array: number[]) => {
 'let s be the length of the longeset string';
 'let a be the length of the array';
 // now lets work through it in parts
-// sorting through one string is O(s * log s)
-// now we have to do this for every string, and there are (a) total strings.
-// therefore sorting strings is O(a*s log s)
-// right now most candidates would make a huge mistake of saying that sorting the array takes O(a log a) time. BUT you also need to compare the strings - so that means each comparison takes O(s) time, so there are O(a log a) comparisons that take O(s) time each. This means it will take O(a*s log a) time to complete the sorting of the sorted strings. In total it will take: O(a*s log a) + O(a*s log s) = O(a*s(log a + log s)) time
+  // sorting through one string is O(s * log s)
+  // now we have to do this for every string, and there are (a) total strings.
+  // therefore sorting strings is O(a*s log s)
+  // right now most candidates would make a huge mistake of saying that sorting the array takes O(a log a) time. BUT you also need to compare the strings - so that means each comparison takes O(s) time, so there are O(a log a) comparisons that take O(s) time each. This means it will take O(a*s log a) time to complete the sorting of the sorted strings. In total it will take: O(a*s log a) + O(a*s log s) = O(a*s(log a + log s)) time
+
+
 
 //9-------------------------------------------------------------------------
 // sums up all the values of the nodes in a blanaced binary search tree, what is the runtime?
@@ -146,7 +155,7 @@ const binarySum = (node: any): any => {
     return 0;
   }
   return binarySum(node.left) + node.value + binarySum(node.right);
-};
+}
 // can think about this the most straightforward way, this code touches each node once and does a constant time amount of work with each "touch" (excluding the recursive calls). So runtime would be linear with respect to the number of nodes in the tree, if there are N nodes, the runtime will be O(N)
 
 // we also said that runtime for recursive calls is typically O(branches^depth), since there are two branches or recursive calls at each call - we are looking at O(2^depth).
@@ -157,6 +166,7 @@ const binarySum = (node: any): any => {
 // 2logN = N
 // threfore complexity is O(N) complexity where N is the number of Nodes.
 
+
 //10-------------------------------------------------------------------------
 // the following checks if a number is prime by checking the divisability on numbers less than it. Only needs to go up to the square root of n because if n is divisible by a number greater than its square root then it is divisible by something smaller than it
 // for examples while 33 is divided by 11 (which is greater than sqrt(33)) the counterpart to 11 is 3 (3*11===33). 33 will have already been eliminated as a prime number by 3
@@ -164,61 +174,155 @@ const binarySum = (node: any): any => {
 const isPrime = (n: number): boolean => {
   for (let x: number = 2; x * x <= n; x++) {
     if (n % x === 0) {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 // the work inside the loop is constant O(1), therefore we just care about how many iterations the loop must go through in worst case. The for loop will start when x = 2 and end when x * x >= n, or stops when x = sqrt(n).
 // the loop is really something like this
 const isPrime2 = (n: number): boolean => {
   for (let x: number = 2; x <= Math.sqrt(n); x++) {
     if (n % x === 0) {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 // complexity is sqrt(N) or N^(1/2)
+
+
 
 //11-------------------------------------------------------------------------
 // the following code computes n! what is the time complexity?
 const factorial = (n: number): number => {
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n == 0 ) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    return n * factorial(n-1);
   }
-};
-// the first two statements are constant time O(1), it calls itself recursively once. since its not a recursive function with multiple branches we cant say it follows O(branches^depth) model.
+}
+// the first two statements are constant time O(1), it calls itself recursively once. since its not a recursive funciton with multiple branches we cant say it follows O(branches^depth) model.
 // Think about it it will recurse from n to (n-1) to (n-2) down to 1. Therefore it will take O(n) time.
+
+
 
 //12-------------------------------------------------------------------------
 // this prints all perumations of a string with all distinct characters
 let permutation = (str: string): void => {
-  permutations(str, '');
-};
+  permutations(str, '')
+}
 const permutations = (str: string, prefix: string): void => {
   if (str.length === 0) {
-    console.log(prefix); // takes O(N) time since each line needs to log
+    console.log(prefix) // takes O(N) time since each line needs to log
   } else {
     for (let i = 0; i < str.length; i++) {
       // lines below will take O(N) time combined, due to string concat
       // notice that lengths of rem, prefix, and str.charAt(i) will always be n
       let rem = str.substring(0, i) + str.substring(i + 1);
-      permutations(rem, prefix + str.charAt(i));
+      permutations(rem, prefix + str.charAt(i))
     }
   }
-};
+}
+
 // each node in the call tree then corresponds to O(n) work, but how many times does this function get called?
 // picture a call tree representing our function for a string like 'abcd'
 //                  ___________root
 //                 |
 //      ___________a____________      just showing one branch (a)
 //     |           |            |
-//    _ab_        _ac_         _ad_
+//    _ab_        _ac_         _ad_ 
 // abc   abd    acb   acd     adb  adc
 //  |     |      |     |       |    |
 // abcd  abdc   acbd  acdb    adbc adcb
+// How many leaf nodes are there? (nodes with no children) we branch four times at the root (a, b, c, d) then three times, then twice, then once. This is expressed as 4*3*2*1 or 4! or n! permutations
+// how many total nodes are there, each leaf node is attached to a path with n nodes in this example each leaf has 4, which is n, levels above it. So at most there are n * n! total nodes in the tree.
+// so the runtime is - since it's doing O(N) work, there are N! left nodes, therefore n*n! total nodes it is total nodes * work => O(n*n*n!) which is expressed as O((N+2)!) [not the same as O(N!)]
+
+
+
+//13-------------------------------------------------------------------------
+// the following computes the Nth Fibonacci number
+const fib = (n: number): number => {
+  if (n <= 0) {
+    return 0
+  } else if (n === 1) {
+    return 1
+  }
+  return fib(n-1) + fib(n-2)
+}
+// O(branches^depth) for recursive function, looks like branches is 2 and depth is n. Thinking about it, the function does work O(1) for the if and else if statements. The function must go through the equation a total of n-1 times. this would mean O(2^n-1) or just O(2^n). Explanation: two branches per call, and we go as deep as N, therefore runtimes is O(2^n)
+// generally when you see an algorithm with muliple recursive calls, you're looking at exponential runtime
+
+
+//14-------------------------------------------------------------------------
+// the following prints all Fibonacci numbers from 0 to n. What is the time complexity?
+const fibo = (n: number): number => {
+  if (n <= 0) {
+    return 0
+  } else if (n === 1) {
+    return 1
+  }
+  return fib(n-1) + fib(n-2)
+}
+
+const allFib = (n: number): void => {
+  for (let i = 0; i < n; i++) {
+    console.log(i + ': ' + fibo(i))
+  }
+}
+// starting with allFib, the function does O(N) work, because it does the log once for each value, but also it calls fibo() each time. fibo itself is a recursive function O(branches^depth) and there are two branches and the deps is from n to 0 so n, so fibo is O(2^n) and allFib is O(n) so its O(n*2^n). WRONG. This is because while the outside function is O(N) time complexity, n is decreasing each subsequent call.
+// lets walk through it
+// fib(1) -> 2^1 steps
+// fib(2) -> 2^2 steps
+// fib(3) -> 2^3 steps
+// fib(4) -> 2^4 steps
+// so total work is 2^1, 2^2, 2^3, 2^4, 2^5 ... 2^n
+// this simplifies to 2^(n+1) - 2 work so the algorithm is then still O(2^n)
+
+
+
+//15-------------------------------------------------------------------------
+// the following prints all Fibonacci numbers from 0 to n. This time, it stores previously computed values in an integer array. If it has already been computed, it just returns the cache. What is the runtime?
+const fibon (n: number, memo: number[]) {
+  if (n <= 0) {
+    return 0
+  } else if (n <= 1) {
+    return 1
+  } else if (memo[n] > 0) {
+    return memo[n];
+  }
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
+
+const allFib2 = (n: number): void {
+  let memo: number[] = [n+1]
+  for (let i = 0; i < n; i ++) {
+    console.log(i + ': ' + fibon(i, memo))
+  }
+}
+
+// walk through what the algorithm does:
+// fibon(0) -> since n<=0, return 0
+// fibon(1) -> since n<=1, return 1
+// fibon(2) -> memo[2] = fibon(1) + fibon(0)
+  // fibon(1) -> since n<=1, return 1
+  // fibon(0) -> since n<=1, return 0
+  // store 1 at memo[2]
+// fibon(3) -> memo[3] = fibon(2) + fibon(1)
+  // fibon(2) -> lookup memo[2], return 1
+  // fibon(1) -> return 1;
+  // store 2 at memo[3]
+// fibon(4) -> memo[4] = fibon(3) + fibon(2)
+  // fibon(3) -> lookup memo[3], return 2
+  // fibon(2) -> lookup memo[2], return 1
+  // store 3 at memo[4]
+// fibon(5) -> memo[5] = fibon[4] + fibon[3]
+  // fibon(4) -> lookup memo[4], return 3
+  // fibon(3) -> lookup memo[3], return 2
+  // store 5 at memo[5]
+
+// at each call to fibon(i) we have already computed the storeed values for fibon(i-1) & fibon(i-2), we just have to look these up, store the new result and return. This takes a constant amount of time. We do a constant amount of work a constant amount of time so this is O(n) time. This technique, called memorization is a very common one to optimize exponenetial time recursive algorithms
