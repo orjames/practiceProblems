1efc4f071042c015ce2c1137cf409bb090626cb0
orjames <orjames@berkeley.edu>
2019-06-14 18:33:40 -0700

    updated questions

--- technicalQuestions.ts
+++ technicalQuestions.ts
@@ -218,7 +218,7 @@ const zeroMatrix = (matrix: number[][]) => {
 
 zeroMatrix([[1, 1, 1, 1], [1, 1, 0, 1], [1, 1, 1, 1]]);
 
-// assume you hav ea method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring eg "waterbottle" is a rotation of "erbottlewat"
+// assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring eg "waterbottle" is a rotation of "erbottlewat"
 const isRotation = (s1, s2) => {
   if (s1.length !== s1.length && s1.length > 0) {
     return false;
